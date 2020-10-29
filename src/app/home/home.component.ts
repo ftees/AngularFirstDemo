aad1e9fb53055f9164bd64646f3a943a5709ffa4
ftees <tiennp8@gmail.com>
2020-10-29 11:51:43 +0700

    four

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MyServiceService } from '../my-service.service';
+interface Phone{
+  id: String,
+  name:String,
+  brand:String,
+  color:String,
+  price:Number,
+  description:String,
+  cre_date:String,
+  image:String
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +17,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  list:Phone[];
+  list2:Phone[] = [];
+  list3:Phone[] = [];
+  constructor(private service:MyServiceService) {
 
-  constructor() { }
-
+   }
+  
   ngOnInit(): void {
+     this.list = this.service.getPhone();
+     this.list.sort((a, b) => (a.cre_date < b.cre_date) ? 1 : -1)
+     for(var i=0;i<4;i++){
+        this.list2.push(this.list[i]);
+     }
+     for(var k=this.list.length-5;k<this.list.length-1;k++){
+       this.list3.push(this.list[k]);
+     }
   }
 
 }
