f8c0706735558bc344f17f163c445ac75f073e91
ftees <tiennp8@gmail.com>
2020-12-13 21:48:12 +0700

    update about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import {CommonModule} from "@angular/common";
 import { DetailComponent } from './detail/detail.component';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
-
+import {AboutComponent} from './about/about.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent,pathMatch: 'full' },
   { path: 'detail/:id',component:DetailComponent},
+  {path:'about',component:AboutComponent},
   { path: 'product/:brand', component: ProductComponent, children:[
   { path: 'detail/:id',component:DetailComponent}
   ] },
